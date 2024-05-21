26bb8e675e4d92e0efb5bb7807d9209f7df41687
Ahmed Mohamed <Ahmed.Mohamed@mssa.newhorizons.com>
2024-05-21 10:29:42 -0600

    Jaggad Array

--- Mod1JaggedarrayDemo/Program.cs
+++ Mod1JaggedarrayDemo/Program.cs
@@ -10,9 +10,31 @@ namespace Mod1JaggedarrayDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(" Jagged Array Demo");
-            Console.WriteLine("Jagged");
-
+            Console.WriteLine("Jagged Array Demo");
+            int[][] temp = new int[4][]; // the first array must have a number or value 
+            for (int i = 0; i < temp.Length; i++)
+            {
+                Console.WriteLine($"Enter the number of readings for City{i}");
+                int readinges=int.Parse(Console.ReadLine());
+                temp[i]= new int[readinges]; // this locating size for the horizontal array.
+                Console.WriteLine($" Enter {readinges} for City {i}");
+                for (int j = 0; j < readinges; j++)
+                {
+                    Console.Write($"temp of City[{i}][{j}]: ");
+                    temp[i][j] = int.Parse(Console.ReadLine()); // get the temp 
+                }
+            }
+            Console.WriteLine("\nDisplating reading: ");
+            for (int i = 0;i < temp.Length; i++)
+            {
+                Console.Write($"City {i} : ");
+                for (int j = 0;j < temp[i].Length; j++)
+                {
+                    Console.Write(temp[i][j] + "F\t ");
+                }
+                Console.WriteLine(" ");
+            }
+            Console.ReadKey();
         }
     }
 }
